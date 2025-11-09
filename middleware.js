a11e0a519513c716df0ee6daf958ef35ff670141
dhruvjain2004 <dhruvjain527@gmail.com>
2025-11-09 11:05:01 +0530

    some middleware change

--- middleware.js
+++ middleware.js
@@ -2,6 +2,7 @@ import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
+
 const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
   "/account(.*)",
@@ -49,5 +50,6 @@ export const config = {
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
     "/(api|trpc)(.*)",
+    runtime: 'nodejs',
   ],
 };
